a514c2c0658df3c8d26942c2510c1d11c097bfc2
root <classic.nct@gmail.com>
2023-08-21 14:42:13 +0700

    Update nexus #3

--- src/classes/Nexus.groovy
+++ src/classes/Nexus.groovy
@@ -6,13 +6,13 @@ class Nexus {
     String CREDENTIAL_ID
     String ARTIFACT_VERSION
 
-    public Nexus(String URL, String PROTOCOL, String VERSION, 
-    String REPOSITORY, String CREDENTIAL_ID, String ARTIFACT_VERSION) {
-        this.URL = URL;
-        this.PROTOCOL = PROTOCOL;
-        this.VERSION = VERSION;
-        this.REPOSITORY = REPOSITORY;
-        this.CREDENTIAL_ID = CREDENTIAL_ID;
-        this.ARTIFACT_VERSION = ARTIFACT_VERSION;
-    }
+    // public Nexus(String URL, String PROTOCOL, String VERSION, 
+    // String REPOSITORY, String CREDENTIAL_ID, String ARTIFACT_VERSION) {
+    //     this.URL = URL;
+    //     this.PROTOCOL = PROTOCOL;
+    //     this.VERSION = VERSION;
+    //     this.REPOSITORY = REPOSITORY;
+    //     this.CREDENTIAL_ID = CREDENTIAL_ID;
+    //     this.ARTIFACT_VERSION = ARTIFACT_VERSION;
+    // }
 }
